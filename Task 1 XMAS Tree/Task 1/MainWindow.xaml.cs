c1db792c873b40b4d8ff7d2afbe7ee071980100a
ToniShoosh <toni.sustar0@gmail.com>
2023-10-23 00:20:21 +0200

    Initial commit & First file upload

---
+++ Task 1 XMAS Tree/Task 1/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void GenerateButtonClicked(object sender, RoutedEventArgs e)
+        {
+            outputTextBox.Text = "";
+
+            outputTextBox.Text += "\n";
+
+            outputTextBox.Text += HeaderInputTextBox.Text;
+
+            outputTextBox.Text += "\n\n";
+
+            int rows = int.Parse(rowsTextBox.Text);
+
+            GenerateTree(rows);
+
+            outputTextBox.Text += "\n";
+
+            outputTextBox.Text += FooterInputTextBox.Text;
+
+            outputTextBox.Text += "\n\n";
+        }
+        private int GenerateTree(int a)
+        {
+            int space = a - 1;
+
+            for (int i = 1; i < space; i++)
+            {
+                outputTextBox.Text += " ";
+                if (i + 1 == space)
+                {
+                    outputTextBox.Text += "*\n";
+                }
+            }
+
+            int midspace = 1;
+           
+            space--;
+
+            for(int sp = space; sp > 1; sp--)
+            {
+                
+                for(int i = 1; i < sp; i++)
+                {
+                    outputTextBox.Text += " ";
+                    if (i + 1 == sp)
+                    {
+                        outputTextBox.Text += "*";
+                        
+                    }
+                }
+
+                for (int j = midspace; j > 0; j--)
+                {
+                    
+                    outputTextBox.Text += " ";
+                    
+                    
+                    if (j == 1)
+                    {
+                        outputTextBox.Text += "*\n";
+                        
+                    }
+                }
+
+                midspace += 2;
+
+
+            }
+
+            midspace += 2;
+
+            for(int i = midspace; i > 0; i--)
+            {
+               
+                 outputTextBox.Text += "*";
+
+            }
+
+            outputTextBox.Text += "\n";
+
+            space++;
+
+            for (int i = 1; i < space; i++)
+            {
+                outputTextBox.Text += " ";
+                if (i + 1 == space)
+                {
+                    outputTextBox.Text += "*\n";
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Task 2 Boxes/Task 2/Task 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///[10, x, y, z]    x = sirina, y = duzina, z = visina
+
+            int[,] boxesArray = new int[10, 3];
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Initial boxes: ");
+
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    boxesArray[i, j] = rnd.Next(1, 11);
+                    Console.Write(" " + boxesArray[i, j]);
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+
+
+
+
+        }
+
+        void Max_Visina(int[,] boxesArray)
+        {
+            int[,] tempBoxes = new int[10, 3];
+
+            tempBoxes[0, 2] = boxesArray[0,2];
+
+            for (int i = 0; i < 10; i++)
+            {
+               for(int j=0; i < 3; j++)
+                {
+
+                }   
+
+            }
+
+        }
+
+        void swap(int[,] boxesArray, int[,] tempBoxes, int i, int j)
+        {
+           
+        }
+    }
+}
