118e194f2ee174c19042bdaaadec0a18365722f7
ToniShoosh <toni.sustar0@gmail.com>
2023-11-03 06:12:14 +0100

    Added a case for non-rotated boxes max stack

--- Task 2 Boxes/Task 2/Task 2/Program.cs
+++ Task 2 Boxes/Task 2/Task 2/Program.cs
@@ -13,10 +13,10 @@ namespace Task_2
         public int width;
         public int height;
         public int base_area;
-        
+
         public Box(int id, int length, int width, int height)
         {
-            this.id = id; 
+            this.id = id;
             this.length = length;
             this.width = width;
             this.height = height;
@@ -33,11 +33,10 @@ namespace Task_2
             Random rnd = new Random();
 
             Console.WriteLine("Initial boxes: ");
-
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
-                /// x = length, y = width, z = height
-                
+                // x = length, y = width, z = height
+
                 int x = rnd.Next(1, 11);
                 int y = rnd.Next(1, 11);
                 int z = rnd.Next(1, 11);
@@ -50,39 +49,66 @@ namespace Task_2
                 boxesArr[10 + i] = new Box(i, z, y, x);
                 boxesArr[20 + i] = new Box(i, z, x, y);
 
-                Console.Write("ID: " + boxesArr[10 + i].id + "  Dimensions: " + boxesArr[10 + i].length + " " + boxesArr[10 + i].width + " " + boxesArr[10 + i].height + "\n");
-                Console.Write("ID: " + boxesArr[20 + i].id + "  Dimensions: " + boxesArr[20 + i].length + " " + boxesArr[20 + i].width + " " + boxesArr[20 + i].height + "\n");
+                Console.Write("ID: " + boxesArr[10 + i].id + "  Dimensions: " + boxesArr[10 + i].length + " " + boxesArr[10 + i].width + " " + boxesArr[10 + i].height + " Rotated box \n");
+                Console.Write("ID: " + boxesArr[20 + i].id + "  Dimensions: " + boxesArr[20 + i].length + " " + boxesArr[20 + i].width + " " + boxesArr[20 + i].height + " Rotated box \n");
             }
             Console.Write("\n");
 
+
+            //Case for non-rotated boxes array
+
+            Box[] nonRotatedBoxesArr = new Box[10];
+
+            //New array for 
+            for(int i = 0; i < 10; i++)
+            {
+                nonRotatedBoxesArr[i] = boxesArr[i];
+            }
+
+            Array.Sort(nonRotatedBoxesArr, (box1, box2) => box2.base_area.CompareTo(box1.base_area));
+
+            int boxCount = nonRotatedBoxesArr.Length;
+
+            Console.Write("\nMax stack height for non-rotated boxes:\n");
+
+            Max_Height(nonRotatedBoxesArr, boxCount);
+
+            boxCount = boxesArr.Length;
+
+            //Sort boxes by their base area
+
             Array.Sort(boxesArr, (box1, box2) => box2.base_area.CompareTo(box1.base_area));
 
-            Console.Write("Sorted array\n");
-            for (int i = 0; i < 30; i++)
+            Console.Write("\nSorted array\n");
+            for (int i = 0; i < boxCount; i++)
             {
                 Console.Write("ID: " + boxesArr[i].id + "  Dimensions: " + boxesArr[i].length + " " + boxesArr[i].width + " " + boxesArr[i].height + " " + boxesArr[i].base_area + "\n");
             }
 
-            Max_Height(boxesArr);
+
+            //TODO Case where rotated boxes can be used but only a single instance of the same box is used in the max stack
+
+            Console.Write("\nCase where multiple instances of a box can be used and boxes can be rotated:\n");
+
+            Max_Height(boxesArr, boxCount);
         }
 
 
-        //Case where there can be multiple instances of a certain box.
-        //TODO There can be only one instance of a certain box.
-        static void Max_Height(Box[] boxesArr)
+        static void Max_Height(Box[] boxesArr, int boxCount)
         {
-            int[] maxArray = new int[30];
-            int[] prevBoxIndex = new int[30];
+            int[] maxArray = new int[boxCount];
+            int[] prevBoxIndex = new int[boxCount];
 
-            for (int i=0; i<30; i++) {
+            for (int i = 0; i < boxCount; i++)
+            {
                 maxArray[i] = boxesArr[i].height;
                 prevBoxIndex[i] = -1;
             }
-            
 
-            for(int i = 1; i < 30; i++)
+
+            for (int i = 1; i < boxCount; i++)
             {
-                for(int j = 0; j < i; j++)
+                for (int j = 0; j < i; j++)
                 {
                     if (boxesArr[i].length <= boxesArr[j].length && boxesArr[i].width <= boxesArr[j].width && maxArray[i] < maxArray[j] + boxesArr[i].height)
                     {
@@ -90,13 +116,13 @@ namespace Task_2
                         prevBoxIndex[i] = j;
                     }
                 }
-                    
+
             }
 
             int max = 0;
             int topBoxIndex = -1;
 
-            for (int i = 0; i < 30; i++)
+            for (int i = 0; i < boxCount; i++)
             {
                 if (maxArray[i] > max)
                 {
@@ -107,9 +133,9 @@ namespace Task_2
 
             if (topBoxIndex != -1)
             {
-                Console.WriteLine("\nMax stack height for a case where multiple instances of a certain box can be used: " + max);
+                Console.WriteLine("\nMax height for stacked boxes: " + max);
 
-                
+
                 Stack<int> stack = new Stack<int>();
                 while (topBoxIndex != -1)
                 {
@@ -132,6 +158,6 @@ namespace Task_2
 
         }
 
-        
+
     }
 }
